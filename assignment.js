32326c4f184cc12944e4a9c8cc7d5e272537e176
Arman-Bin-Juyel <tahinjr555@gmail.com>
2024-05-31 00:21:12 +0600

    assignment

---
+++ assignment.js
@@ -0,0 +1,60 @@
+// 1 create a variable
+
+let carName = "volvo";
+console.log(carName)
+
+// 2 one single line, declar three variable
+
+let firstName1 = "John";
+let lastName1 = "Doe";
+let age = 35;
+
+// 3 correct assignment operator
+
+x = 10;
+y = 5;
+x *= y;
+
+// 4. corect datatype
+
+let length = 16; //number datatype
+let lastNames = "Johnson" //string datatype
+
+const fullName = {
+    firstName2: "John",
+    lastName2: "son"
+}; //object
+
+
+// 5. Execute the function named
+
+function myFunction() {
+    alert("Hello World");
+}
+//myFunction();
+
+// 6. Create an object called person
+const person = {
+  name: "John", age: 50};
+//alert(person.name + " is " + person.age);
+
+// 7. <button> element
+
+//<button onclick="alert('Hello')">Click me.</button> 
+
+// 8. Array Related Question
+
+const cars = ["Volvo", "Jeep", "Mercedes"];
+cars[0] = "Ford";
+
+// 9. Math Related Problems
+
+let cmm = Math.random();  //create a random number
+let x3 = Math.max(10, 20); //the largest number of 10 and 20
+let x4 = Math.sqrt(9);   //the square root of 9.
+
+// 10. comparison operator related problems! 
+
+x5 = 10;
+y2 = 5;
+//alert(x > y);
